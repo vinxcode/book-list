2cf6312bbbe08c8f6ef17363f2cd59df3669feb9
Kevin <vincitbussiness@gmail.com>
2024-06-09 23:01:08 -0600

    Fetching data correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { useState, useEffect } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('./db.json')
+        const datos = await response.json()
+        setData(datos.library)
+        console.log(data)
+      } catch (error) {
+        console.error(error, 'Ya la carlitos, amix')
+      }
+    }
+    fetchData()
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Here are some books you can choose</h1>
+      {
+        data.map((dato, index) => (
+          <div key={index}>
+            <p>{ dato.book.title }</p>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
